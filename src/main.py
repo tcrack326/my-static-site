6d09f081a207d71f1a927f83e0dadd385a0b4e82
tcrack326 <thomas.alan.mccracken@gmail.com>
2025-05-30 16:06:13 -0400

    correcting build

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     print(f"{sys.argv}")
-    basepath = '/' if len(sys.argv) > 1 else sys.argv[1]
+    basepath = '/' if len(sys.argv) < 1 else sys.argv[1]
     print(f"the basepath: {basepath}")
     copy_static_to_public()
     generate_pages_recursive('content', 'template.html', 'docs', basepath)
